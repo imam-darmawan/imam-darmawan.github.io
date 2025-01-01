b21a4218cf18a9d3809b227342b02f8255be3d7a
Imam Darmawan <79206804+imam-darmawan@users.noreply.github.com>
2025-01-01 21:58:23 +0700

    Update content

--- src/data/author.js
+++ src/data/author.js
@@ -16,11 +16,11 @@ export default {
         }, */
         {
           title: "formstudio.site Clone",
-          url: "https://id-portfolio-formstudio.pages.dev",
+          url: "https://id-formstudio-clone.pages.dev",
           date: 2024,
           category: "clone",
-          description: `This is a clone and modified version of <a href="#">FF Identity</a> Framer template. Design and concept are copyrighted to <a href="#">Favorit x Frame</a>. If you like the template you can buy by following the link.`,
-          source: "https://github.com/imam-darmawan/formstudio-clone",
+          description: `This website is a modified clone version to pure code of the <a href="https://the-brandidentity.com/store/product/ff-identity-framer-template-ff-grotesk-typeface-bundle" rel="noreferrer noopener" target="_blank">FF Identity Framer template</a>. Design and concept are credited to <a href="https://www.favoritframe.com" rel="noreferrer noopener" target="_blank">Favorit x Frame</a>. If you like the template, you can purchase it from Favorit x Frame by following the link. Note: I am not affiliated with Favorit x Frame.`,
+          source: "https://github.com/imam-darmawan/formstudio.site-clone",
           libs: ["javascript", "react", "gsap", "tailwindcss"],
         },
         {
@@ -28,6 +28,8 @@ export default {
           url: "https://themeskibou-yuuga.pages.dev",
           date: 2024,
           category: "theme",
+          description:
+            "Yuuga is a premium, elegant, and modern theme crafted for your Ghost publication. Whether you prefer an elegant or a modern look, Yuuga offers the features to create a visually stunning and professional website.",
           libs: [
             "javascript",
             "handlebars",
@@ -37,29 +39,28 @@ export default {
           ],
         },
         {
-          title: "Monster Truck",
-          url: "https://id-portfolio-monstertruck.pages.dev",
+          title: "Monster Truck Offroad",
+          url: "https://id-monster-truck-offroad.pages.dev",
           date: 2023,
           category: "game",
           libs: ["gdscript", "godot"],
         },
         {
           title: "TypingRate",
-          url: "https://id-portfolio-typingrate.pages.dev",
+          url: "https://id-typingrate.pages.dev",
           date: 2023,
           category: "game",
-          // libs: ["javascript"],
+          libs: ["javascript"],
         },
       ],
     },
-    designer: {
-      about:
-        "Additionally, I like visual arts. This page showcases my works in this field.",
+    freelancer: {
+      about: "This page is under construction 🚧",
       projects: [],
     },
   },
   socialLinks: [
-    { label: "upwork", url: "https://upwork.com" },
+    // { label: "upwork", url: "https://upwork.com" },
     { label: "github", url: "https://github.com/imam-darmawan" },
   ],
   profileImage: "/tupai-100x100.png",
